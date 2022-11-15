4910c2974e43506a63ee0fe02616fff4d3c066e4
Jaana Dominique Tabalon <jaanatabalon@gmail.com>
2022-11-15 14:28:57 -0500

    Moving files and copied Customer Information button onto screen

--- front-end/src/components/ModalForm.vue
+++ front-end/src/components/ModalForm.vue
@@ -50,6 +50,39 @@ export default {
       </div>
     </Transition>
   </form>
+
+  <template>
+    <Transition name="modal">
+      <div
+        v-if="show"
+        class="modal-mask"
+      >
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <slot name="header">default header</slot>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body">default body</slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer">
+                default footer
+                <button
+                  class="modal-default-button"
+                  @click="$emit('close')"
+                >
+                  OK
+                </button>
+              </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </template>
 </template>
 
 <style>
@@ -71,7 +104,7 @@ export default {
 }
 
 .modal-container {
-  width: 300px;
+  width: 500px;
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
